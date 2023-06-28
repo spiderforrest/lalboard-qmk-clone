dde52825f03adc98fd13c544f3546717af7e436b
spiderforrest <spider@spood.org>
2023-06-28 14:25:55 -0700

    started on utility layer

--- keyboards/handwired/lalboard/keymaps/spiderforrest/keymap.c
+++ keyboards/handwired/lalboard/keymaps/spiderforrest/keymap.c
@@ -1,4 +1,4 @@
-/* Copyright 2021 Ben Gruver <jesusfreke@jesusfreke.com>
+/* Copyright 2021 Ben Gruver <jesusfreke@jesusfreke.com> {{{
  * 2023 Spider Forrest <lalboard-qmk-clone@spood.org>
  *
  * Permission is hereby granted, free of charge, to any person obtaining a
@@ -18,7 +18,7 @@
  * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
  * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
  * IN THE SOFTWARE.
- */
+}}} */
 
 /*  {{{ keyboard effort calc config
  *  https://colemakmods.github.io/mod-dh/analyze.html
@@ -88,6 +88,25 @@ matrix_simple
 ═══════════════════════════════════════
 }}} */
 
+// {{{ layer template
+//    [LAYER] = LAYOUT(
+//        // Center       North           East            South           West
+//        _______,        _______,        _______,        _______,        _______,                    // Right Index
+//        _______,        _______,        _______,        _______,        _______,                    // Right Middle
+//        _______,        _______,        _______,        _______,        _______,                    // Right Ring
+//        _______,        _______,        _______,        _______,        _______,                    // Right Pinky
+//
+//        // Center       North           East            South           West
+//        _______,        _______,        _______,        _______,        _______,                    // Left Pinky
+//        _______,        _______,        _______,        _______,        _______,                    // Left Ring
+//        _______,        _______,        _______,        _______,        _______,                    // Left Middle
+//        _______,        _______,        _______,        _______,        _______,                    // Left Index
+//
+//        // Down         Inner           Upper           Outer Upper     Outer Lower     Follow Through
+//        _______,        _______,        _______,        _______,        _______,        _______,    // Right Thumb
+//        _______,        _______,        _______,        _______,        _______,        _______     // Left Thumb
+//    )
+// }}}
 
 #include QMK_KEYBOARD_H
 
@@ -95,28 +114,12 @@ matrix_simple
 #include <stdint.h>
 
 enum my_keycodes {
-  KC_NORMAL_HOLD = SAFE_RANGE,
-  KC_FUNC_HOLD
-  //
-  //KC_GAME_HOLD,
-  //KC_GAZE,
+    _ = SAFE_RANGE,
 };
 
 enum layer {
-  NORMAL,
-  DANSEN,
-  NORMAL_HOLD,
-  FUNC,
-  FUNC_HOLD,
-  NAS,
-  //COLEMAK,
-
-  // COLEMAK_HOLD,
-  // GAME,
-  // GAME_HOLD,
-  // GAME_MOD_1,
-  // GAME_MOD_2,
-  NUM_LAYERS
+    DANSEN,
+    SYMOUSE,
 };
 
 #define KC_RIGHT_CURLY KC_RIGHT_CURLY_BRACE
@@ -125,134 +128,44 @@ enum layer {
 
 /* Declared weak so that it can easily be overridden. */
 __attribute__((weak)) const uint16_t PROGMEM keymaps[NUM_LAYERS][MATRIX_ROWS][MATRIX_COLS] = {
-  [NORMAL] = LAYOUT(
-    /*Center           North           East            South           West*/
-    /*R1*/ KC_J,            KC_U,           KC_QUOTE,       KC_M,           KC_H,
-    /*R2*/ KC_K,            KC_I,           KC_COLON,       KC_COMMA,       KC_Y,
-    /*R3*/ KC_L,            KC_O,           KC_LGUI,         KC_DOT,      KC_N,
-    /*R4*/ KC_SCOLON,       KC_P,           KC_BSLASH,      KC_SLASH,       KC_RBRC,
+    [DANSEN] = LAYOUT(
+        // Center       North           East            South           West
+        KC_N,           KC_L,           KC_ESC,         KC_H,           KC_DOT,                    // Right Index
+        KC_E,           KC_U,           KC_SLASH,       KC_K,           KC_QUOTE,                  // Right Middle
+        KC_I,           KC_Y,           KC_SCOLON,      KC_M,           KC_MINUS,                  // Right Ring
+        KC_O,           KC_BSLASH,      KC_RBRC,        KC_J,           KC_LEFT,                   // Right Pinky
 
-    /*L1*/ KC_F,            KC_R,           KC_G,           KC_V,           KC_DOUBLE_QUOTE,
-    /*L2*/ KC_D,            KC_E,           KC_T,           KC_C,           KC_GRAVE,
-    /*L3*/ KC_S,            KC_W,           KC_B,           KC_X,           KC_ESC,
-    /*L4*/ KC_A,            KC_Q,           KC_LBRC,        KC_Z,           KC_DEL,
+        // Center       North           East            South           West
+        KC_T,           KC_Q,           KC_PIPE,        KC_V,           KC_LBRC,                   // Left Pinky
+        KC_S,           KC_X,           KC_V,           KC_W,           _______, // no idea tbh    // Left Ring
+        KC_R,           KC_C,           KC_B,           KC_D,           _______, // maybe util osl // Left Middle
+        KC_A,           KC_F,           KC_G,           KC_P,           KC_COMMA,                  // Left Index
 
-    /*Down                  Inner           Upper           Outer Upper     Outer Lower*/
-    /*RT*/ MO(NAS),         KC_SPACE,       TO(FUNC),       KC_BSPACE,      KC_LALT,  _______, //TG(NAS),
-    /*LT*/ KC_LSHIFT,       KC_ENTER,       KC_NORMAL_HOLD, KC_TAB,         KC_LCTRL, KC_CAPS
-  ),
-  [DANSEN] = LAYOUT(
-    /*Center                North           East            South           West*/
-    /*R1*/ KC_N,            KC_L,           KC_ESC,         KC_H,           KC_DOT,
-    /*R2*/ KC_E,            KC_U,           KC_SLASH,       KC_K,           KC_QUOTE,
-    /*R3*/ KC_I,            KC_Y,           KC_SCOLON       KC_M            KC_MINUS
-    /*R4*/ KC_O,            KC_BSLASH,      KC_RBRC,        KC_J,           KC_LEFT,
 
-    /*L1*/ KC_T,            KC_Q,           KC_PIPE,        KC_V,           KC_LBRC,
-    /*L2*/ KC_S,            KC_X,           KC_V,           KC_W,           _______,
-    /*L3*/ KC_R,            KC_C,           KC_B,           KC_D,           _______,
-    /*L4*/ KC_A,            KC_F,           KC_G,           KC_P,           KC_COMMA,
+        // Down         Inner           Upper           Outer Upper     Outer Lower     Follow Through
+        KC_TAB,         KC_BSPACE       layer,          DC_DELE,        KC_LALT,        _______,   // Right Thumb
+        KC_ENTER,       KC_SPACE,       layer,          KC_TAB,         KC_LCTRL,       _______    // Left Thumb
+    ),
+    [DART] = LAYOUT(
+        // Center       North           East            South           West
+        // arrow keys   nav cluster     audio/media     mouse move      mouse wheel
+        KC_LEFT,        KC_HOME,        KC_MUTE,        KC_MS_L,        KC_WH_L,                    // Right Index
+        KC_DOWN,        KC_PGDN,        KC_VOLD,        KC_MS_D,        KC_WH_U,                    // Right Middle
+        KC_UP,          KC_PGUP,        KC_VOLU,        KC_MS_U,        KC_WH_D,                    // Right Ring
+        KC_RIGHT,       KC_END,         KC_F22,         KC_MS_R,        KC_WH_R,                    // Right Pinky
 
-    /*Down                  Inner           Upper           Outer Upper     Outer Lower*/
-    /*RT*/ MO(NAS),         KC_SPACE,       TO(FUNC),       KC_BSPACE,      KC_LALT, _______,
-    /*LT*/ KC_LSHIFT,       KC_ENTER,       KC_NORMAL_HOLD, KC_TAB,         KC_LCTRL, _______
-  ),
+        // Center       North           East            South           West
+        _______,        _______,        _______,        _______,        _______,                    // Left Pinky
+        KC_7,           KC_4,           _______,        KC_1,           _______,                    // Left Ring
+        KC_8,           KC_5,           _______,        KC_2,           _______,                    // Left Middle
+        KC_9,           KC_6,           _______,        KC_3,           _______,                    // Left Index
 
-  [NORMAL_HOLD] = LAYOUT(
-    /*Center           North           East            South           West*/
-    /*R1*/ KC_LEFT,         LCTL(KC_UP),   LCTL(KC_RIGHT), LCTL(KC_DOWN), LCTL(KC_LEFT),
-    /*R2*/ KC_UP,           KC_MS_U,        KC_MS_R,        KC_MS_D,        KC_MS_L,
-    /*R3*/ KC_DOWN,         KC_WH_U,        KC_WH_R,        KC_WH_D,        KC_WH_L,
-    /*R4*/ KC_RIGHT,        XXXXXXX,        XXXXXXX,        XXXXXXX,        XXXXXXX,
-
-    /*L1*/ RGUI(KC_H),      XXXXXXX,        XXXXXXX,        KC_BTN1,        LCTL(KC_V),
-    /*L2*/ XXXXXXX,         XXXXXXX,        XXXXXXX,        KC_BTN2,        XXXXXXX,
-    /*L3*/ XXXXXXX,         XXXXXXX,        XXXXXXX,        KC_BTN3,        XXXXXXX,
-    /*L4*/ DF(NORMAL),      _______,        _______,        XXXXXXX/*DF(DANSEN)*/,       _______,
-
-    /*Down             Inner           Upper           Outer Upper     Outer Lower*/
-    /*RT*/ _______,         _______,        _______,        _______,        _______, _______,
-    /*LT*/ _______,         _______,        _______,        _______,        _______, _______
-  ),
-
-  [NAS] = LAYOUT(
-    /*Center           North           East            South           West*/
-    /*R1*/ KC_7,            KC_AMPR,        KC_UNDS,        KC_KP_PLUS,     KC_6,
-    /*R2*/ KC_8,            KC_KP_ASTERISK, KC_COLON,       KC_COMMA,       KC_CIRCUMFLEX,
-    /*R3*/ KC_9,            KC_LPRN,        KC_LGUI,        KC_DOT,         KC_SCOLON,
-    /*R4*/ KC_0,            KC_RPRN,        XXXXXXX,        KC_QUES,        KC_RBRC,
-
-    /*L1*/ KC_4,            KC_DOLLAR,      KC_5,           KC_MINUS,       KC_SLASH,
-    /*L2*/ KC_3,            KC_HASH,        KC_GT,          KC_PERCENT,     KC_LT,
-    /*L3*/ KC_2,            KC_AT,          XXXXXXX,        KC_X,           KC_ESC,
-    /*L4*/ KC_1,            KC_EXCLAIM,     KC_TILDE,       KC_EQUAL,       KC_DEL,
-
-    /*Down                  Inner           Upper           Outer Upper     Outer Lower*/
-    /*RT*/ MO(NAS),         KC_SPACE,       TO(FUNC),       KC_BSPACE,      KC_LALT, _______,
-    /*LT*/ KC_LSHIFT,       KC_ENTER,       KC_NORMAL_HOLD, KC_TAB,         KC_LCTRL, _______
-  ),
-
-  [FUNC] = LAYOUT(
-    /*Center           North           East            South           West*/
-    /*R1*/ KC_HOME,         KC_UP,          KC_RIGHT,       KC_DOWN,        KC_LEFT,
-    /*R2*/ XXXXXXX,         KC_F8,          XXXXXXX,        KC_F7,          KC_END,
-    /*R3*/ KC_PSCREEN,      KC_F10,         KC_LGUI,        KC_F9,          KC_INS,
-    /*R4*/ KC_PAUSE,        KC_PGUP,        KC_F12,         KC_PGDN,        KC_F11,
-
-    /*L1*/ KC_HOME,         KC_UP,          KC_RIGHT,       KC_DOWN,        KC_LEFT,
-    /*L2*/ XXXXXXX,         KC_F6,          XXXXXXX,        KC_F5,          XXXXXXX,
-    /*L3*/ XXXXXXX,         KC_F4,          XXXXXXX,        KC_F3,          KC_ESC,
-    /*L4*/ XXXXXXX,         KC_F2,          XXXXXXX,        KC_F1,          KC_DEL,
-
-    /*Down             Inner           Upper           Outer Upper     Outer Lower*/
-    /*RT*/ MO(NAS),         KC_SPACE,       TO(FUNC),       KC_BSPACE,      KC_LALT, _______,
-    /*LT*/ KC_LSHIFT,       KC_ENTER,       KC_NORMAL_HOLD, KC_TAB,         KC_LCTRL,_______
-  ),
-
-  [FUNC_HOLD] = LAYOUT(
-    /*Center           North           East            South           West*/
-    /*R1*/ KC_LEFT,         LCTL(KC_UP),   LCTL(KC_RIGHT), LCTL(KC_DOWN), LCTL(KC_LEFT),
-    /*R2*/ KC_UP,           KC_MS_U,        KC_MS_R,        KC_MS_D,        KC_MS_L,
-    /*R3*/ KC_DOWN,         KC_WH_U,        KC_WH_R,        KC_WH_D,        KC_WH_L,
-    /*R4*/ KC_RIGHT,        XXXXXXX,        XXXXXXX,        XXXXXXX,        XXXXXXX,
-
-    /*L1*/ XXXXXXX,         XXXXXXX,        XXXXXXX,        LCTL(KC_V),     XXXXXXX,
-    /*L2*/ XXXXXXX,         XXXXXXX,        XXXXXXX,        LCTL(KC_C),     XXXXXXX,
-    /*L3*/ XXXXXXX,         XXXXXXX,        XXXXXXX,        LCTL(KC_X),     XXXXXXX,
-    /*L4*/ _______,      _______,        _______,        _______,       _______,
-
-    /*Down             Inner           Upper           Outer Upper     Outer Lower*/
-    /*RT*/ _______,         _______,        _______,        _______,        _______,_______,
-    /*LT*/ _______,         _______,        _______,        _______,        _______, _______
-  ),
-
+        // Down         Inner           Upper           Outer Upper     Outer Lower     Follow Through
+        _______,        _______,        _______,        _______,        _______,        _______,    // Right Thumb
+        _______,        _______,        _______,        _______,        _______,        _______     // Left Thumb
+    )
 };
 
 
-bool process_record_user(uint16_t keycode, keyrecord_t *record) {
-  switch (keycode) {
-
-    case KC_NORMAL_HOLD:
-      if (record->event.pressed) {
-        layer_clear();
-        layer_on(NORMAL_HOLD);
-        SEND_STRING(SS_LCTL(SS_LGUI(SS_LSFT(SS_TAP(X_F19)))));
-      } else {
-        layer_off(NORMAL_HOLD);
-      }
-      return false;
-    case KC_FUNC_HOLD:
-      if (record->event.pressed) {
-        layer_clear();
-        layer_on(FUNC_HOLD);
-        //SEND_STRING(SS_LCTL(SS_LGUI(SS_LSFT(SS_TAP(X_F19)))));
-      } else {
-        layer_off(FUNC_HOLD);
-      }
-      return false;
-    default:
-      return true;
-  }
-};
 
 // vim:foldmethod=marker
